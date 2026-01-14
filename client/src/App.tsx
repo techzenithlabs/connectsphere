18a9ae64c346c893206f548d614080bcc9b874af
=yogeshraghav-dev <techzenithlabs@gmail.com>
2026-01-14 22:41:24 +0530

    added new structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,16 @@
-import type { JSX } from 'react';
-import {Routes,Route,Navigate} from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-function Private({ children }: { children: JSX.Element }) {
-  const token = localStorage.getItem("cs_access");
-  return token ? children : <Navigate to="/login" replace />;
-}
+import { Routes,Route} from "react-router-dom"
+import HomePage from "./pages/public/HomePage"
+import PageNotFound from "./pages/pageNotFound"
 
 function App() {
 
   return (
-    <Routes>
-       <Route path="/login" element={<LoginPage />} />
-      <Route
-        path="/app"
-        element={
-          <Private>
-            <div style={{ padding: 24 }}>You’re logged in 🎉</div>
-          </Private>
-        }
-      />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+    <div>
+      <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ client/src/pages/public/HomePage.tsx
@@ -0,0 +1,5 @@
+export default function HomePage(){
+    return (
+        <div><h1>Welcome to ConnectSphere</h1></div>
+    )
+}
