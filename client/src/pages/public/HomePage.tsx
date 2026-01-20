ad5674d661fe9189f13b1bcf7777ebc36213f75f
=yogeshraghav-dev <techzenithlabs@gmail.com>
2026-01-20 23:39:33 +0530

    aded css

--- client/src/pages/public/HomePage.tsx
+++ client/src/pages/public/HomePage.tsx
@@ -1,6 +1,5 @@
 import HeroSection from '../../components/home/HeroSection';
 import Header from '../../components/includes/Header';
-import '../../styles/HomePage.css';
 
 export default function HomePage(){
     return (
